09054ef06395b318657b5ed21efe901dc05483c7
gorills <gorivanickiy@gmail.com>
2025-12-28 11:59:57 +0700

    Remove non-existent RemoveCrossOriginOpenerPolicyMiddleware from MIDDLEWARE

--- voice_recorder/settings.py
+++ voice_recorder/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'recordings.middleware.RemoveCrossOriginOpenerPolicyMiddleware',  # Удаление COOP для HTTP
     'recordings.middleware.CreateUserSettingsMiddleware',  # Автоматическое создание настроек
 ]
 
