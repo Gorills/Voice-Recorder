71bf12d40044542fbc3c9bd38d3959879859325d
gorills <gorivanickiy@gmail.com>
2025-12-28 11:45:31 +0700

    Add middleware to remove Cross-Origin-Opener-Policy header for HTTP requests

--- voice_recorder/settings.py
+++ voice_recorder/settings.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'recordings.middleware.RemoveCrossOriginOpenerPolicyMiddleware',  # Удаление COOP для HTTP
     'recordings.middleware.CreateUserSettingsMiddleware',  # Автоматическое создание настроек
 ]
 
